4a33a433b0e8b0646257049d7d9db2b5251f2ef5
Elsy Chavez <00125717@uca.edu.sv>
2020-06-16 23:34:44 -0600

    Cambios para labo 7, repository

--- labo5/src/main/java/com/uca/capas/service/EstudianteServiceImpl.java
+++ labo5/src/main/java/com/uca/capas/service/EstudianteServiceImpl.java
@@ -9,35 +9,39 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.uca.capas.dao.EstudianteDAO;
 import com.uca.capas.domain.Estudiante;
+import com.uca.capas.repositories.EstudianteRepo;
 
 @Service
 public class EstudianteServiceImpl implements EstudianteService {
 	
 	@Autowired
-	EstudianteDAO estudianteDAO;
+	EstudianteRepo estudianteRepo;
+	//EstudianteDAO estudianteDAO;
 
 	@Override
 	public List<Estudiante> findAll() throws DataAccessException {
-		return estudianteDAO.findAll();
+		//return estudianteDAO.findAll();
+		return estudianteRepo.findAll();
 	}
 
 	@Override
 	public Estudiante findOne(Integer code) throws DataAccessException {
-		return estudianteDAO.findOne(code);
+		//return estudianteDAO.findOne(code);
+		return estudianteRepo.getOne(code);
 	}
 
 	@Override
 	@Transactional
 	public void save(Estudiante estudiante) throws DataAccessException {
-		estudianteDAO.save(estudiante);
-		
+		//estudianteDAO.save(estudiante);
+		estudianteRepo.save(estudiante);
 	}
 
 	@Override
 	@Transactional
 	public void delete(Integer codigoEstudiante) throws DataAccessException {
-		estudianteDAO.delete(codigoEstudiante);
-		
+		//estudianteDAO.delete(codigoEstudiante);
+		estudianteRepo.deleteById(codigoEstudiante);
 	}
 
 }
